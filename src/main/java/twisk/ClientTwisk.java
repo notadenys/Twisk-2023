d094019d7af3120f1ab2a116aed308b838eb679f
OSADTSIV Ivan <ivan.osadtsiv8@etu.univ-lorraine.fr>
2024-02-12 17:45:57 +0100

    SasEntree update, added FabriquerNumero

---
+++ src/main/java/twisk/ClientTwisk.java
@@ -0,0 +1,4 @@
+package main.java.twisk;
+public class ClientTwisk {
+
+}
--- src/main/java/twisk/simulation/Simulation.java
+++ src/main/java/twisk/simulation/Simulation.java
@@ -1,6 +1,11 @@
 package main.java.twisk.simulation;
+import main.java.twisk.monde.Monde;
 public class Simulation {
+    public Monde monde;
     public Simulation() {
+        monde = new Monde();
+    }
+    public void simuler() {
 
     }
 }
