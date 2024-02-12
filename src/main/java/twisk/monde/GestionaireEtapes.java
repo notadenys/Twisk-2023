e8e5e7947032ed6a6e27ba756e4874c933eed16a
notadenys <sh.denys.ev@gmail.com>
2024-02-12 16:12:33 +0100

    update GestionaireEtapes

--- src/main/java/twisk/monde/GestionaireEtapes.java
+++ src/main/java/twisk/monde/GestionaireEtapes.java
@@ -1,17 +1,34 @@
 package main.java.twisk.monde;
 
-public class GestionaireEtapes {
-    public GestionaireEtapes() {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
+public class GestionaireEtapes implements Iterable<Etape> {
+    private final ArrayList<Etape> etapes;
+
+    public GestionaireEtapes() {
+        etapes = new ArrayList<>();
     }
 
     public void ajouter(Etape...etapes) {
-
+        this.etapes.addAll(Arrays.asList(etapes));
     }
 
     public int nbEtapes() {
-        return 0;
+        return etapes.size();
     }
 
+    @Override
+    public Iterator<Etape> iterator()
+    {
+        return etapes.iterator();
+    }
 
+    @Override
+    public String toString() {
+        return "GestionaireEtapes{" +
+                "etapes=" + etapes +
+                '}';
+    }
 }
