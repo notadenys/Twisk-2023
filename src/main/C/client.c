c7a65028f24a14901fda22d3fe5bf1e498eb1864
OSADTSIV Ivan <ivan.osadtsiv8@etu.univ-lorraine.fr>
2024-02-13 16:27:49 +0100

    added C files

---
+++ src/main/C/client.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void delai(int temps, int delta) { // 0 < delta < temps < 100
+
+}
+
+void entrer(int etape) {
+
+}
+
+void transferer(int source, int destination) {
+
+}
+
+void simulation(int ids) {
+    
+}
