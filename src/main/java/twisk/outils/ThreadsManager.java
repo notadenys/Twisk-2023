3a4f1a8a61394bc49fe20135a51557aa255158dd
notadenys <sh.denys.ev@gmail.com>
2024-05-26 17:34:52 +0200

    created ThreadsManager

---
+++ src/main/java/twisk/outils/ThreadsManager.java
@@ -0,0 +1,42 @@
+package twisk.outils;
+
+import javafx.concurrent.Task;
+
+import java.util.ArrayList;
+
+public final class ThreadsManager {
+    // The field must be declared volatile so that double check lock would work correctly.
+    private static volatile ThreadsManager instance;
+
+    private final ArrayList<Thread> threads;
+
+    private ThreadsManager() {
+        threads = new ArrayList<>();
+    }
+
+    public static ThreadsManager getInstance() {
+        ThreadsManager result = instance;
+        if (result != null) {
+            return result;
+        }
+        // Thread safe creation of instance
+        synchronized (ThreadsManager.class) {
+            if (instance == null) {
+                instance = new ThreadsManager();
+            }
+            return instance;
+        }
+    }
+
+    public void lancer(Task<Void> task) {
+        Thread thread = new Thread(task);
+        threads.add(thread);
+        thread.start();
+    }
+
+    public void detruireTout() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+}
